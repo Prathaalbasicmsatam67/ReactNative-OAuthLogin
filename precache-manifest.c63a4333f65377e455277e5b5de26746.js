814b81a23ce98ee2a6a84fe9cd0a8c3070b452a0
prathamesh.satam <prathamesh.satam@ril.com>
2020-06-26 13:34:14 +0530

    Updates

---
+++ precache-manifest.c63a4333f65377e455277e5b5de26746.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99c7c088ddf40621b79c79a51d9edb26",
+    "url": "/ReactNative-OAuthLogin/index.html"
+  },
+  {
+    "revision": "958100317aedf217b369",
+    "url": "/ReactNative-OAuthLogin/static/css/15.53785d6a.chunk.css"
+  },
+  {
+    "revision": "3bc557ca3826c8b80076",
+    "url": "/ReactNative-OAuthLogin/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "60135827e0d079a2e9ca",
+    "url": "/ReactNative-OAuthLogin/static/js/13.ea858a8a.chunk.js"
+  },
+  {
+    "revision": "8baa8727df1982ec4620a918911df65a",
+    "url": "/ReactNative-OAuthLogin/static/js/13.ea858a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7731a7426b7c792ed8b",
+    "url": "/ReactNative-OAuthLogin/static/js/14.1757e013.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactNative-OAuthLogin/static/js/14.1757e013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958100317aedf217b369",
+    "url": "/ReactNative-OAuthLogin/static/js/15.b9f7f025.chunk.js"
+  },
+  {
+    "revision": "df4e1ac0766bff39309a",
+    "url": "/ReactNative-OAuthLogin/static/js/16.ee27ee4a.chunk.js"
+  },
+  {
+    "revision": "3bc557ca3826c8b80076",
+    "url": "/ReactNative-OAuthLogin/static/js/main.f1887b53.chunk.js"
+  },
+  {
+    "revision": "2c95567e8d5224b73063",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerDailyMotion.946a10d4.chunk.js"
+  },
+  {
+    "revision": "9670abbd922082155e5d",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerFacebook.fc512122.chunk.js"
+  },
+  {
+    "revision": "3bec61b9ddef0c5f7821",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerFilePlayer.e5f8ae16.chunk.js"
+  },
+  {
+    "revision": "f366882534ea5a77f6c3",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerMixcloud.0cb9fd01.chunk.js"
+  },
+  {
+    "revision": "80ac21c2376a456e6069",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerSoundCloud.7a010775.chunk.js"
+  },
+  {
+    "revision": "c3145a7acf44fceab40f",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerStreamable.cc6e0595.chunk.js"
+  },
+  {
+    "revision": "98fd3e74cac646dadfa9",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerTwitch.b4934972.chunk.js"
+  },
+  {
+    "revision": "48d6fa4460924f75d896",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerVidyard.b7be5eeb.chunk.js"
+  },
+  {
+    "revision": "4efdbce3b145cd2b9c45",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerVimeo.7bfa7d02.chunk.js"
+  },
+  {
+    "revision": "84b296e5d27949a7528e",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerWistia.e161c321.chunk.js"
+  },
+  {
+    "revision": "1980669c49276765708e",
+    "url": "/ReactNative-OAuthLogin/static/js/reactPlayerYouTube.b043b746.chunk.js"
+  },
+  {
+    "revision": "6032dc24cab2474c1a86",
+    "url": "/ReactNative-OAuthLogin/static/js/runtime-main.7a35c034.js"
+  }
+]);
